1ebcacd0ef65d3e1c6f6ab35b28bf6a64a060561
Glenn Rudge <glennrudge@outlook.com>
2020-08-22 19:27:23 -0400

    Made changes to Windows GUI.

--- SchoolFormsApp/Form1.cs
+++ SchoolFormsApp/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,5 +17,24 @@ namespace SchoolFormsApp
             InitializeComponent();
         }
 
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
