d4f4f93eeb40cb39e890ee35878ef42330f7ed37
Glenn Rudge <glennrudge@outlook.com>
2020-08-22 07:59:59 -0400

    initial commit

--- SchoolLibrary/School.cs
+++ SchoolLibrary/School.cs
@@ -6,11 +6,71 @@ namespace SchoolLibrary
 {
     class School
     {
-        string SchoolName;
-        string SchoolAddress;
-        string SchoolCity;
-        string SchoolState;
-        string SchoolZip;
-        string PhoneNumber;
-    }
-}
+ 
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string PhoneNumber { get; set; }
+
+        private string _twitterAddress;
+        public string TwitterAddress
+        {
+            // Make sure the twittwe address starts with an @ symbol
+            get { return _twitterAddress; }
+            set
+            {
+                if(value.StartsWith("@"))
+                {
+                    _twitterAddress = value;
+                } else
+                {
+                    throw new Exception("The twitter address must begin with @.");
+
+                }
+            }
+        }
+
+        // Baisc Constructor
+        public School()
+        {
+            Name = "Untitled School";
+            PhoneNumber = "555-1234";
+        }
+
+        // Overloaded Constructor
+        public School(string SchoolName, string SchoolPhoneNumber)
+        {
+            Name = SchoolName;
+            PhoneNumber = SchoolPhoneNumber;
+        }
+
+        //public float AverageThreeScores(float a, float b, float c)
+        //{
+        //    var result = (a + b + c) / 3;
+        //    return result;
+        //}
+
+        public static float AverageThreeScores(float a, float b, float c) => (a + b + c) / 3;
+
+        public static int AverageThreeScores(int a, int b, int c)
+        {
+            var result = (a + b + c) / 3;
+            return result;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine(this.Name);
+            sb.AppendLine(this.Address);
+            sb.AppendLine(this.City);
+            sb.Append(", ");
+            sb.Append(this.State);
+            sb.Append(this.Zip);
+
+            return sb.ToString();
+        }
+    } // Class
+} // Namespace
